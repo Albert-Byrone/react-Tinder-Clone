d253fad73505e004f4396d9f84e05e0a77ddf0a9
Albert-Byrone <albertbyrone1677@gmail.com>
2021-12-28 16:28:13 +0300

    fetch the data from the firebase database

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -2,20 +2,18 @@ import React, { useState } from "react";
 import TinderCard from "react-tinder-card";
 import "./TinderCards.css";
 function TinderCards() {
-  const [people, setPeople] = useState([
-    {
-      name: "Mary Kom",
-      url: "https://upload.wikimedia.org/wikipedia/commons/d/dc/Mary_Kom_-_British_High_Commission%2C_Delhi%2C_27_July_2011.jpg",
-    },
-    {
-      name: "Jobe Steve",
-      url: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTY2MzU3OTcxMTUwODQxNTM1/steve-jobs--david-paul-morrisbloomberg-via-getty-images.jpg",
-    },
-    {
-      name: "Maria Sharapova",
-      url: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/00/Maria_Sharapova_Australian_Open_Players%27_Party_2015.jpg/800px-Maria_Sharapova_Australian_Open_Players%27_Party_2015.jpg",
-    },
-  ]);
+  const [people, setPeople] = useState([]);
+
+  // a piece of code that runs based on a condition
+  useEffect(() => {
+    database
+      .collection("people")
+      .onSnapshot((snapshot) =>
+        setPeople(snapshot.docs.map((doc) => doc.data()))
+      );
+
+    // runs once if left blank
+  }, []);
   return (
     <div>
       <div className="tinderCards__cardContainer">
