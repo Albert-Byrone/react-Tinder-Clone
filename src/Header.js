0c52d83efb021f6051e8aba370264045659b0e9b
Albert-Byrone <albertbyrone1677@gmail.com>
2021-12-29 13:19:31 +0300

    add links to the buttons

--- src/Header.js
+++ src/Header.js
@@ -2,22 +2,38 @@ import React from "react";
 import PersonIcon from "@material-ui/icons/Person";
 import ForumIcon from "@material-ui/icons/Forum";
 import IconButton from "@material-ui/core/IconButton";
+import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
 import "./Header.css";
 
-function Header() {
+import { Link, useNavigate } from "react-router-dom";
+
+function Header({ backButton }) {
+  const history = useNavigate();
   return (
     <div className="header">
-      <IconButton>
-        <PersonIcon className="header__icon" fontSize="large" />
-      </IconButton>
-      <img
-        className="header__logo"
-        src="https://1000logos.net/wp-content/uploads/2018/07/Tinder-icon.png"
-        alt="tinder icon"
-      />
-      <IconButton>
-        <ForumIcon className="header__icon" fontSize="large" />
-      </IconButton>
+      {backButton ? (
+        <IconButton onClick={() => history("/", { backButton })}>
+          <ArrowBackIosIcon className="header__icon" fontSize="large" />
+        </IconButton>
+      ) : (
+        <IconButton>
+          <PersonIcon className="header__icon" fontSize="large" />
+        </IconButton>
+      )}
+
+      <Link to="/">
+        <img
+          className="header__logo"
+          src="https://1000logos.net/wp-content/uploads/2018/07/Tinder-icon.png"
+          alt="tinder icon"
+        />
+      </Link>
+
+      <Link to="/chat">
+        <IconButton>
+          <ForumIcon className="header__icon" fontSize="large" />
+        </IconButton>
+      </Link>
     </div>
   );
 }
