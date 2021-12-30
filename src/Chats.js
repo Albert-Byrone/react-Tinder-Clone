8a8eedb2176f7e8c956c23f37d236b42f1516745
Albert-Byrone <albertbyrone1677@gmail.com>
2021-12-30 12:14:32 +0300

    update the data

--- src/Chats.js
+++ src/Chats.js
@@ -12,25 +12,25 @@ function Chats() {
         profilepic="https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5ed560d07fe4060006bbce1e%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D422%26cropX2%3D1300%26cropY1%3D0%26cropY2%3D879"
       />
       <Chat
-        name="Albert Byrone"
+        name="Indomi Sweet"
         message="Yoo whats up"
         timestamp=" 40 seconds ago"
         profilepic="https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5ed560d07fe4060006bbce1e%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D422%26cropX2%3D1300%26cropY1%3D0%26cropY2%3D879"
       />
       <Chat
-        name="Albert Byrone"
+        name="Loner Byrone"
         message="Yoo whats up"
         timestamp=" 40 seconds ago"
         profilePic="https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5ed560d07fe4060006bbce1e%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D422%26cropX2%3D1300%26cropY1%3D0%26cropY2%3D879"
       />
       <Chat
-        name="Albert Byrone"
+        name="Albert Lonner"
         message="Yoo whats up"
         timestamp=" 40 seconds ago"
         profilePic="https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5ed560d07fe4060006bbce1e%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D422%26cropX2%3D1300%26cropY1%3D0%26cropY2%3D879"
       />
       <Chat
-        name="Albert Byrone"
+        name="Chris Okur"
         message="Yoo whats up"
         timestamp=" 40 seconds ago"
         profilePic="https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5ed560d07fe4060006bbce1e%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D422%26cropX2%3D1300%26cropY1%3D0%26cropY2%3D879"
