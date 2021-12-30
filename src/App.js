1051184aff7e911bc3038f73e1a4b0cf2a6b98a0
Albert-Byrone <albertbyrone1677@gmail.com>
2021-12-30 12:08:46 +0300

    add login route

--- src/App.js
+++ src/App.js
@@ -4,11 +4,31 @@ import Header from "./Header";
 import TinderCards from "./TinderCards";
 import SwipeButons from "./SwipeButons";
 import Chats from "./Chats";
+import ChatScreen from "./ChatScreen";
+import Login from "./Login";
 
 function App() {
   return (
     <div className="App">
       <Routes>
+        <Route
+          path="/login"
+          element={
+            <>
+              <Header backButton="/" />
+              <Login />
+            </>
+          }
+        />
+        <Route
+          path="/chat/:person"
+          element={
+            <>
+              <Header backButton="/chat" />
+              <ChatScreen />
+            </>
+          }
+        />
         <Route
           path="/chat"
           element={
