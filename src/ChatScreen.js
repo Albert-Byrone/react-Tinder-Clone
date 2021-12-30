ad7fcb06e5acc403c02eb4e60a1617d92f15899b
Albert-Byrone <albertbyrone1677@gmail.com>
2021-12-30 11:35:54 +0300

    create the  receiver chat area

---
+++ src/ChatScreen.js
@@ -0,0 +1,8 @@
+          <div className="chatScreen__message">
+            <Avatar
+              className="chatScreen__image"
+              alt={message.name}
+              src={message.image}
+            />
+            <p className="chatScreen__text">{message.message}</p>
+          </div>
