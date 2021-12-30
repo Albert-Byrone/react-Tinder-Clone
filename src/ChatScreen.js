804ab399afbddf805a024e10762478b7f5a646ba
Albert-Byrone <albertbyrone1677@gmail.com>
2021-12-30 11:36:40 +0300

    add the  form to accept inpot from the user

--- src/ChatScreen.js
+++ src/ChatScreen.js
@@ -11,3 +11,22 @@
             <p className="chatScreen__textUser">{message.message}</p>
           </div>
         )
+      <form className="chatScreen__input">
+        <input
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          className="chatScreen__inputField"
+          type="text"
+          placeholder="Type a message"
+        />
+        <button
+          type="submit"
+          onClick={handleSend}
+          className="chatScreen__inputButton "
+        >
+          Send{" "}
+        </button>
+      </form>
+    </div>
+  );
+}
