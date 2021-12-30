c27ba6d2d6d012e18357cba9ac1d592ab2bc6c4f
Albert-Byrone <albertbyrone1677@gmail.com>
2021-12-30 11:38:08 +0300

    minor imports

--- src/ChatScreen.js
+++ src/ChatScreen.js
@@ -1,3 +1,9 @@
+import React, { useState } from "react";
+import Avatar from "@material-ui/core/Avatar";
+import "./ChatScreen.css";
+
+function ChatScreen() {
+  const [input, setInput] = useState("");
   const [message, setMessage] = useState([
     {
       name: "Albert Byrone",
@@ -20,6 +26,11 @@
     setMessage([...message, { message: input }]);
     setInput(""); 
   };
+  return (
+    <div className="chatScreen">
+      <p className="chatScreen__timestamp">You matched with Albert</p>
+      {message.map((message) =>
+        message.name ? (
           <div className="chatScreen__message">
             <Avatar
               className="chatScreen__image"
@@ -33,6 +44,8 @@
             <p className="chatScreen__textUser">{message.message}</p>
           </div>
         )
+      )}
+
       <form className="chatScreen__input">
         <input
           value={input}
@@ -52,3 +65,5 @@
     </div>
   );
 }
+
+export default ChatScreen;
